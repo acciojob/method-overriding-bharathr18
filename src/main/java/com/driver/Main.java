06e8be19bfda06c4f2ed11b2b836bb868dcba317
Bharath R <bharathr6362@gmail.com>
2022-12-12 19:47:10 +0530

    Made meth public

--- src/main/java/com/driver/Main.java
+++ src/main/java/com/driver/Main.java
@@ -19,6 +19,8 @@ public class Main {
     }
     public static void main(String args[])
     {
+        A a = new A();
+        System.out.println(a.meth());
         B b = new B();
         System.out.println(b.meth());
     }
